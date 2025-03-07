7988bf487eb9ec2c4cb2d0366761d82cbbe58d50
Itezaz ul Hassan <hassannasir098@gmail.com>
2025-03-08 01:33:58 +0500

    change s3 config

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,7 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+console.log(process.env.S3_URL, process.env.S3_BUCKET, process.env.S3_REGION, process.env.S3_ACCESS_KEY_ID, process.env.S3_SECRET_ACCESS_KEY)
 
 module.exports = defineConfig({
   projectConfig: {
@@ -21,21 +22,21 @@ module.exports = defineConfig({
   },
   plugins: [
     'medusa-payment-manual',
-    {
-      resolve: `medusa-file-s3`,
-      options: {
-          s3_url: process.env.S3_URL,
-          bucket: process.env.S3_BUCKET,
-          region: process.env.S3_REGION,
-          access_key_id: process.env.S3_ACCESS_KEY_ID,
-          secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-          cache_control: process.env.S3_CACHE_CONTROL,
-          // optional
-          download_file_duration:
-            process.env.S3_DOWNLOAD_FILE_DURATION,
-          prefix: process.env.S3_PREFIX,
-      },
-    },
+    // {
+    //   resolve: `medusa-file-s3`,
+    //   options: {
+    //       s3_url: process.env.S3_URL,
+    //       bucket: process.env.S3_BUCKET,
+    //       region: process.env.S3_REGION,
+    //       access_key_id: process.env.S3_ACCESS_KEY_ID,
+    //       secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+    //       // cache_control: process.env.S3_CACHE_CONTROL,
+    //       // optional
+    //       // download_file_duration:
+    //       //   process.env.S3_DOWNLOAD_FILE_DURATION,
+    //       // prefix: process.env.S3_PREFIX,
+    //   },
+    // },
   ],
   modules: [
     {
@@ -56,6 +57,26 @@ module.exports = defineConfig({
         redis: {
           url: process.env.REDIS_URL,
         },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              // other options...
+            },
+          },
+        ],
       },
     },
   ],
