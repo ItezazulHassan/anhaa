f7a2b70ba66510d310e905438fa1fe646e0d2aaa
Itezaz ul Hassan <hassannasir098@gmail.com>
2025-03-07 23:13:49 +0500

    s3 file changes

--- medusa-config.ts
+++ medusa-config.ts
@@ -20,7 +20,22 @@ module.exports = defineConfig({
     backendUrl: process.env.MEDUSA_BACKEND_URL,
   },
   plugins: [
-    'medusa-payment-manual'
+    'medusa-payment-manual',
+    {
+      resolve: `medusa-file-s3`,
+      options: {
+          s3_url: process.env.S3_URL,
+          bucket: process.env.S3_BUCKET,
+          region: process.env.S3_REGION,
+          access_key_id: process.env.S3_ACCESS_KEY_ID,
+          secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+          cache_control: process.env.S3_CACHE_CONTROL,
+          // optional
+          download_file_duration:
+            process.env.S3_DOWNLOAD_FILE_DURATION,
+          prefix: process.env.S3_PREFIX,
+      },
+    },
   ],
   modules: [
     {
